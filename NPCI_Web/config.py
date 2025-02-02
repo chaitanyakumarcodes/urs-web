8df894764afeba4b321fa95035cc87afc97e661e
chaitanyakumarcodes <140366194+chaitanyakumarcodes@users.noreply.github.com>
2025-02-02 18:36:43 +0530

    Delete NPCI_Web directory

--- NPCI_Web/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:'ChaitanyA@23'@localhost/<DB_NAME>"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
--- NPCI_Web/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-# Initialize Blueprints for modular routing
-auth_bp = Blueprint("auth", __name__)
-vendor_bp = Blueprint("vendor", __name__)
